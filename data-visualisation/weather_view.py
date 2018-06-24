236ecaa9f8e5425deddf6f39a4392a50e4b35324
chrisharris000 <chris2145.ch@gmail.com>
2018-06-24 20:39:55 +1000

    basic version of final output (working)

--- data-visualisation/weather_view.py
+++ data-visualisation/weather_view.py
@@ -14,10 +14,14 @@ def update_options(*args):
             menu.add_command(label=area, command=lambda locality=area: area_var.set(locality))
 
 def update_summary(*args):
-        current_weather = get_current_weather(region_weather_summary, region_var, area_var)
-        start_var.set(current_weather[0])
-        end_var.set(current_weather[1])
-        precis_var.set(current_weather[5])
+        week_weather = get_week_weather(region_weather_summary, region_var, area_var)
+        r = 10
+        for day in week_weather:
+                max_temp_var = StringVar()
+                max_temp_var.set(day[3])
+                max_temp_lbl = Label(window,textvariable = max_temp_var)
+                max_temp_lbl.grid(row = r, column = 0)
+                r += 1
 
 def update_week_weather(*args):
         week_summary = get_week_weather(region_weather_summary, region_var, area_var)[0]
@@ -31,31 +35,12 @@ def create_window():
     window.geometry('600x600')
     return window
 
-def get_current_weather(region_weather_summary, region_var, area_var):
-        region_weather = region_weather_summary[region_var.get()]
-        for suburb in region_weather:
-                if suburb[0] == area_var.get():
-                        weather_today = suburb[1][0]
-        return weather_today
-
 def get_week_weather(region_weather_summary, region_var, area_var):       
         region_weather = region_weather_summary[region_var.get()]
         for suburb in region_weather:
                 if suburb[0] == area_var.get():
                         week_weather = suburb[1][1:]
         return week_weather
-
-def create_widgets(window, day_summary):
-        lbl_list = []
-        var_list = []
-        for data in day_summary:
-                temp_var = StringVar()
-                temp_var.set(data)
-                lbl = Label(window, textvariable = temp_var)
-                lbl.grid(row = 12 + day_summary.index(data), column = 0)
-                lbl_list.append(lbl)
-                var_list.append(temp_var)
-        return lbl_list,var_list
 
 root = weather_format.import_data()
 REGIONS = weather_format.region_codes()
@@ -67,9 +52,6 @@ window = create_window()
 #create StringVar() objects
 region_var = StringVar()
 area_var = StringVar()
-start_var = StringVar()
-end_var = StringVar()
-precis_var = StringVar()
 
 #create trace for StringVar() 's that need to be updated when dropdow selection changes
 region_var.trace('w', update_options)
@@ -82,24 +64,14 @@ area_optionmenu = OptionMenu(window, area_var, '')
 #initialise StringVar() 's
 region_var.set('Sydney Metropolitan')
 area_var.set("Parramatta")
-current_weather = get_current_weather(region_weather_summary, region_var, area_var)
-start_var.set(current_weather[0])
-end_var.set(current_weather[1])
-precis_var.set(current_weather[5])
 
 #initialise Labels with values
 title_lbl = Label(window, text = "Weather Forecast")
 region_lbl = Label(window, textvariable = region_var)
 area_lbl = Label(window, textvariable = area_var)
 
-curr_weather_lbl = Label(window,text = "Summary of Weather Today")
-start_lbl = Label(window, textvariable = start_var)
-end_lbl = Label(window, textvariable = end_var)
-precis_lbl = Label(window, textvariable = precis_var)
+curr_weather_lbl = Label(window,text = "Summary of Weather for the Week")
 
-#tomorrow Label creation
-week_summary = get_week_weather(region_weather_summary, region_var, area_var)
-lbl_list,var_list = create_widgets(window, week_summary[0])
 
 #place widgets in window
 title_lbl.grid(row = 0, column = 3)
@@ -109,9 +81,6 @@ area_optionmenu.grid(row = 1, column = 1)
 area_lbl.grid(row = 1, column = 4)
 
 curr_weather_lbl.grid(row = 2, column = 3)
-start_lbl.grid(row = 3, column = 0)
-end_lbl.grid(row = 4, column = 0)
-precis_lbl.grid(row = 5, column = 0)
 
 window.mainloop()
 
