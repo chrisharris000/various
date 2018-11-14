cda7b94e5ac570841328e6464c8546ac6bfa8c58
chrisharris000 <chris2145.ch@gmail.com>
2018-11-15 09:45:53 +1100

    initial quote bot

---
+++ twilio/frinkiac.py
@@ -0,0 +1,37 @@
+import schedule
+import requests
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+
+account_sid = ''
+auth_token  = ''
+
+client = Client(account_sid, auth_token)
+
+
+def get_quote():
+    r = requests.get("https://frinkiac.com/api/random")
+    if r.status_code == 200:
+        json = r.json()
+
+        # Combine each line of subtitles into one string.
+        caption = "\n".join([subtitle["Content"] for subtitle in json["Subtitles"]])
+        return caption
+
+def send_MMS():
+    body = get_quote()
+    try:
+        message = client.messages.create(
+            body=body,
+            to="",
+            from_="")
+        
+        print("Message sent!")
+    # If an error occurs, print it out.
+    except TwilioRestException as e:
+        print(e)
+
+schedule.every().day.at("08:00").do(send_MMS)
+
+while True:
+    schedule.run_pending()
